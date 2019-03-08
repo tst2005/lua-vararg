9eaa25028db94333d4694b70a81bbe41757720ef
TsT <tst2005@gmail.com>
2019-03-08 17:16:59 +0100

    updated

--- test.vararg.lua
+++ test.vararg.lua
@@ -1,8 +1,16 @@
 local vararg = require"vararg"
+--local vararg = require "table_pack"
 
+local w = table.pack and table.pack(1, 2, nil, 3, nil, nil)
 local x = vararg(1, 2, 3)
 local y = vararg(1, 2, nil, 3, nil, nil)
 local z = {1, 2, 3}
+
+for i,v in vararg.next, y, nil do
+	print("-", i, v)
+end
+
+print(table.pack and table.unpack(w) or "no table.pack")
 
 assert(vararg.type(x))
 print(x:unpack())
--- vararg.lua
+++ vararg.lua
@@ -1,59 +1,48 @@
 local select = assert(select)
 local table_unpack = assert(table.unpack or _G.unpack)
 
--- varargs1 the complex one
-local exposed_varargs_mt = {}
-exposed_varargs_mt.__metatable=true
 
--- varargs2 the simple one
-local exposed_simple_varargs_mt = {}
-exposed_simple_varargs_mt.__metatable=true
-
--- varargs1
-local varargs_mt = {}
-varargs_mt.__newindex = function() error("readonly object", 2) end
-varargs_mt.__len = function(self)
-	return self[shadowkey]["#"]
-end
-varargs_mt.__metatable = exposed_varargs_mt
-varargs_mt.__ipairs = nil -- TODO
-
--- varargs2
-local simple_varargs_mt = {}
-simple_varargs_mt.__newindex = varargs_mt.__newindex
-simple_varargs_mt.__metatable = exposed_simple_varargs_mt
-
-local shadowkey = {}
-local function varargs_unpack(self, i, j)
-	local shadow = self[shadowkey]
-	return table_unpack(shadow.values, i or 1, j or shadow["#"])
-end
-
-local function mkproxy(...)
-	local len = select("#", ...)
-	if #{...} == len then
-		-- varargs1
-		return setmetatable({unpack=table_unpack, ...}, simple_varargs_mt)
+local function va_next(va, i)
+	if i == nil then
+		i = 1
+	elseif type(i)=="number" and i < (va.n or #va) then
+		i = i+1
 	else
-		-- varargs2
-		local proxy = {
-			unpack = assert(varargs_unpack),
-			[shadowkey] = {["#"]=select("#", ...),values={...}},
-		}
-		return setmetatable(proxy, varargs_mt)
+		return nil
 	end
+	return i, va[i]
+end
+
+local function va_ipairs(t, i)
+	return va_next, t, i
+end
+
+local vararg_mt = {}
+
+vararg_mt.__len = function(self)
+	return assert(self.n)
+end
+vararg_mt.__ipairs = va_ipairs
+
+local function va_pack(...)
+	return setmetatable({unpack=va_unpack, n=select("#", ...), ...}, vararg_mt)
+end
+
+local function va_unpack(self, i, j)
+	--assert(type(x)=="table")
+	return table_unpack(self, i or 1, j or self.n)
+end
+
+local function va_type(x)
+	local x_mt = getmetatable(x)
+	return x_mt and x_mt == vararg_mt and "vararg"
 end
 
 local M = {}
-M.type = function(x)
-	local x_mt = getmetatable(x)
-	return x_mt and ( x_mt == exposed_varargs_mt or x_mt == exposed_simple_varargs_mt ) and "vararg" or nil
-end
-setmetatable(M, {__call = function(_, ...) return mkproxy(...) end})
-M.pack = mkproxy
-M.unpack = function(x, i, j)
-	assert(M.type(x))
-	assert(x.unpack)
-	return x:unpack(i,j)
-end
+M.type   = va_type
+M.pack   = va_pack
+M.unpack = va_unpack
+M.next   = va_next
+M.ipairs = va_ipairs
+setmetatable(M, {__call = function(_, ...) return va_pack(...) end})
 return M
